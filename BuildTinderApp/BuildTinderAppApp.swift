81b973b7574d0ba4f1f00cf40cb65e4eccc304fe
Matthew Brown <52255000+mbrown98@users.noreply.github.com>
2022-09-10 19:38:10 -0400

    project start

---
+++ BuildTinderApp/BuildTinderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildTinderAppApp.swift
+//  BuildTinderApp
+//
+//  Created by Matthew Brown on 9/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BuildTinderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
